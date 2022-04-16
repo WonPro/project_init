2ee7d82472dc6882c8989433b3539287bcf2cc8b
WonPro <wonjunhee.com@gmail.com>
2022-04-16 12:44:46 +0900

    init commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*********************************************************
+*                    Modal Popup Event                   *
+*********************************************************/
+
+  // Open Modal Event
+  // function openPopup(popupName) {
+  //   let modalWrap = document.getElementById('popupWrap');
+  //   let popup = document.getElementById('popup' + popupName);
+
+  //   modalWrap.classList.add('on');
+  //   popup.classList.add('on');
+  // }
+
+
+  // Close Modal Event
+  // function closePopup(popupName) {
+  //   let modalWrap = document.getElementById('popupWrap');
+  //   let popup = document.getElementById('popup' + popupName);
+
+  //   popup.classList.remove('on');
+  //   modalWrap.classList.remove('on');
+  // }
+
+
+
+/*********************************************************
+*                    Document Loaded                     *
+*********************************************************/
+
+$(document).ready(function(){
+
+  
+  // AOS start
+  // AOS.init();
+
+});
